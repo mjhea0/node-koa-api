928c27e2a6cd7faf8be7d10b38725df772175e06
Michael Herman <hermanmu@gmail.com>
2017-08-22 23:28:17 -0600

    init

--- test/sample.test.js
+++ test/sample.test.js
@@ -4,9 +4,10 @@ const chai = require('chai');
 const should = chai.should();
 
 describe('Sample Test', () => {
-  it('should pass', () => {
+  it('should pass', (done) => {
     const sum = 1 + 2;
     sum.should.eql(3);
     sum.should.not.eql(4);
+    done();
   });
 });
